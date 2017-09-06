a4c89c70883751f2017082b8c1d3483f88e4c650
Gilexarr <tmenichel@gmail.com>
2017-09-06 09:41:36 -0400

    Uploading Unity project
    Entire project as well as a script to start the sushi clicker game, it is unfinished

---
+++ SuperSushiSurprise/Assets/Scripts/ClickSushi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSushi : MonoBehaviour {
+
+    BoxCollider2D sushiHitbox;
+
+    private void Start()
+    {
+        sushiHitbox = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray))
+            {
+                sushiHitbox.transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
+            }
+        }
+	}
+}
